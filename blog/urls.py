099d113eaf00ce4ad2562fcb1ea8019ea3655237
Sagi Laniado <slaniado@Group.WilliamHill.PLC>
2020-05-26 21:06:24 +0100

    portfolio commit v3

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
+
+    #if url after 'blog' has an int
+    #then pass this int into blog_id and push to detail function in views
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
